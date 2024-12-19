76843fec9865fb0d0008a83a1c6ff0448505b160
nikitha2000 <nikithajoemon@gmail.com>
2024-12-19 11:34:36 +0530

    resolved

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Form from "../component/UserForm";
+import Form from "../components/UserForm";
 import { User } from "../type/user";
 
 const SignIn = () => {
